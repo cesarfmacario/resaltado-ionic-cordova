6b238cd81520dd20e40ddad73f9fa89c2ebaa930
César Francisco Macario <cesarfmacario@gmail.com>
2019-03-26 09:13:54 -0600

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+    constructor(private platform: Platform) {
+        
+    }
+
+    ionViewWillEnter() {
+        //Verifica si ya se encuentra lista la plataforma  
+        this.platform
+            .ready()
+            .then(() => {
+                //Realiza el llamado al plugin e invoca segun el resultado la funcion correspondiente
+                (<any>window).MiPlugin.saludo('Mundo!!!', this.successCallback, this.errorCallback);
+            });
+    }
+
+    //Funcion para desplegar la respuesta cuando es satisfactorio
+    successCallback(message) {
+        alert(message);
+    }
+
+    //Funcion si hubo un error
+    errorCallback() {
+        alert("Hubo un error");
+    }
+
 
 }
